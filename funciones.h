679443bc0f21ab4057d15d8eb84b53509e5ca01c
marcelcyber <156872798+marcelcyber@users.noreply.github.com>
2024-06-10 16:01:26 -0500

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,4 @@
+void verPersonas(char personas[][4][20]);
+void registrarMatrimonio(char personas[][4][20]);
+void verificarPersona(char personas[][4][20]);
+int buscarPersonaXCedula(char personas[][4][20],char cedula[20]);
